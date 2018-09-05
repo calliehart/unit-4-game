e9c5bf2e5c9870ebb64bc01993e427d634586be1
cartyinacardi <callie_miller@hotmail.com>
2018-09-04 20:23:21 -0500

    first content push

---
+++ assets/javascript/game.js
@@ -0,0 +1,212 @@
+$(document).ready(function(){
+
+    var powerpuff = {
+        startHealth: 140,
+        health: 140,
+        baseAttack: 11,
+        currentAttack: 11,
+        counterAttack: 18
+    }
+
+    var mojojojo = {
+        startHealth: 110,
+        health: 110,
+        baseAttack: 28,
+        currentAttack: 28,
+        counterAttack: 11
+    }
+
+    var bullet = {
+        startHealth: 120,
+        health: 120,
+        baseAttack: 19,
+        currentAttack: 19,
+        counterAttack: 16
+    }
+
+    var him = {
+        startHealth: 150,
+        health: 150,
+        baseAttack: 8,
+        currentAttack: 8,
+        counterAttack: 24
+    }
+
+    var yourChar = null;
+    var defenderChar = null;
+    var defenderID = null;
+    var gameOver = false;
+    var winCounter = 0;
+
+//reset function sets board when site loads
+function reset() {
+    $("#instructions").empty();
+    $("#instructions").append("Start By Selecting Your Character");
+    $("#healthScoreChar").empty();
+    $("#healthScoreDef").empty();
+    $("#powerpuff98").appendTo("#charPool");
+    $("#mojojojo").appendTo("#charPool");
+    $("#bullet").appendTo("#charPool");
+    $("#him").appendTo("#charPool");
+    if (yourChar !== null) {
+        yourChar.currentAttack = yourChar.baseAttack;
+        yourChar = null;
+    }
+    
+    defenderChar = null;
+    defenderID = null;
+    winCounter = 0;
+    gameOver = false;
+    healthReset();
+
+};//ends reset()
+
+reset();
+
+function healthReset() {
+    powerpuff.health = powerpuff.startHealth;
+    mojojojo.health = mojojojo.startHealth;
+    him.health = him.startHealth;
+    bullet.health = bullet.startHealth;
+}
+
+$("#powerpuff98").click(function() {
+
+    if (yourChar === null) {
+        yourChar = powerpuff;
+        $("#powerpuff98").appendTo("#chosenChar");
+        $("#bullet").appendTo("#enemiesPool");
+        $("#mojojojo").appendTo("#enemiesPool");
+        $("#him").appendTo("#enemiesPool");
+        $("#instructions").empty();
+        $("#instructions").append("Now Choose Your Opponent");
+        $("#healthScoreChar").html("Health: " + yourChar.health); 
+       
+
+    } else if (defenderChar === null && yourChar !== powerpuff) {
+       defenderChar = powerpuff;
+       defenderID = "#powerpuff98";
+       $("#instructions").empty();
+       $("#instructions").append("Let The Fight Begin!!");
+       $("#powerpuff98").appendTo("#enemyChar");
+       $("#healthScoreDef").html("Health: " + defenderChar.health);
+    }
+
+});//ends #powerpuff98 click function
+
+$("#bullet").click(function() {
+
+    if (yourChar === null) {
+        yourChar = bullet;
+        $("#bullet").appendTo("#chosenChar");
+        $("#powerpuff98").appendTo("#enemiesPool");
+        $("#mojojojo").appendTo("#enemiesPool");
+        $("#him").appendTo("#enemiesPool");
+        $("#instructions").empty();
+        $("#instructions").append("Now Choose Your Opponent");
+        $("#healthScoreChar").html("Health: " + yourChar.health); 
+       
+    } else if (defenderChar === null && yourChar !== bullet) {
+        defenderChar = bullet;
+        defenderID = "#bullet";
+        $("#instructions").empty();
+        $("#instructions").append("Let The Fight Begin!!");
+        $("#bullet").appendTo("#enemyChar");
+        $("#healthScoreDef").html("Health: " + defenderChar.health);
+    } 
+    
+});//ends #bullet click function
+
+$("#mojojojo").click(function() {
+
+    if (yourChar === null) {
+        yourChar = mojojojo;
+        
+        $("#mojojojo").appendTo("#chosenChar");
+        $("#powerpuff98").appendTo("#enemiesPool");
+        $("#bullet").appendTo("#enemiesPool");
+        $("#him").appendTo("#enemiesPool");
+        $("#instructions").empty();
+        $("#instructions").append("Now Choose Your Opponent");
+        $("#healthScoreChar").html("Health: " + yourChar.health); 
+        
+    } else if (defenderChar === null && yourChar !== mojojojo) {
+        defenderChar = mojojojo;
+        defenderID = "#mojojojo";
+        $("#instructions").empty();
+        $("#instructions").append("Let The Fight Begin!!");
+        $("#mojojojo").appendTo("#enemyChar");
+        $("#healthScoreDef").html("Health: " + defenderChar.health);
+    } 
+    
+});//ends #mojojojo click function
+
+$("#him").click(function() {
+
+    if (yourChar === null) {
+        yourChar = him;
+        
+        $("#him").appendTo("#chosenChar");
+        $("#powerpuff98").appendTo("#enemiesPool");
+        $("#mojojojo").appendTo("#enemiesPool");
+        $("#bullet").appendTo("#enemiesPool");
+        $("#instructions").empty();
+        $("#instructions").append("Now Choose Your Opponent");
+        $("#healthScoreChar").html("Health: " + yourChar.health); 
+        
+    } else if (defenderChar === null && yourChar !== him) {
+        defenderChar = him;
+        defenderID = "#him";
+        $("#instructions").empty();
+        $("#instructions").append("Let The Fight Begin!!");
+        $("#him").appendTo("#enemyChar");
+        $("#healthScoreDef").html("Health: " + defenderChar.health); 
+    } 
+    
+});//ends #him click function
+
+
+
+$("#attackBTN").click(function() {
+    if (gameOver === false) {
+        yourChar.health = yourChar.health - defenderChar.counterAttack;
+        defenderChar.health = defenderChar.health - yourChar.currentAttack;
+        yourChar.currentAttack = yourChar.currentAttack + yourChar.baseAttack;
+
+
+        $("#healthScoreChar").html("Health: " + yourChar.health);
+        $("#healthScoreDef").html("Health: " + defenderChar.health);
+        $("#instructions").html("<h4>You took " + defenderChar.counterAttack + " damage.<br>You're opponent took " + yourChar.currentAttack + " damage.</h4>");
+
+        if (yourChar.health <= 0) {
+            $("#instructions").empty();
+            $("#instructions").append("You Lost!!");
+            gameOver = true;
+            
+        } else if (defenderChar.health <= 0) {
+            winCounter++;
+            if (winCounter === 3){
+                $("#instructions").empty();
+                $("#instructions").append("You Won the Game!");
+                gameOver = true;
+            } else {
+                $("#instructions").empty();
+                $("#instructions").append("You Won This Round. Choose Next Opponent.");
+                defenderChar = null;
+                $(defenderID).appendTo("#enemiesDefeated");
+                defenderID = null;
+            }
+    
+        }
+    }
+
+});//ends attack button
+
+
+$("#resetBTN").click(function() {
+reset();
+
+});
+
+
+});//end of Doc Ready
